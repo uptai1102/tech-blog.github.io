37d152938e87332f6e594eb306b13b9bf991005a
taisei <uptktaiup@gmail.com>
2025-10-30 18:21:39 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://uptai1102.github.io/tech-blog.github.io/";
